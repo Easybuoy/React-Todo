3441ee57525ec08aecc457bb1596675603c35910
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-05-29 13:00:59 +0100

    setup component markup

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TodoForm() {
+  
+  return (
+    <div>
+      <input type="text" placeholder="Enter Task" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+    </div>
+  );
+}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,26 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+export default function TodoList() {
+  const tasks = [
+    {
+      task: "Organize Garage",
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: "Bake Cookies",
+      id: 1528817084358,
+      completed: false
+    }
+  ];
+  return (
+    <div>
+      {tasks.map(task => {
+        return <Todo key={task.id} task={task} />;
+      })}
+    </div>
+  );
+}
